4139e76683e65b0e5f5ced4a98cd549de6d5975a
Thiago Mallon <jamesmallondev@gmail.com>
2018-09-30 17:09:42 -0700

    Adding the transfer-send channel verification

--- routes/channels.php
+++ routes/channels.php
@@ -11,6 +11,4 @@
 |
 */
 
-Broadcast::channel('App.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
-});
+Broadcast::channel('transfer-sent.{receiver}', App\Broadcasting\TransferReceivedChannel::class);
